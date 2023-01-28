6682e40b40622ffc4717b50f28d20f6d6bb0be0d
wwotz <lukeanthonyholland@gmail.com>
2023-01-28 03:17:49 +0000

    added whiteboard and aiboard in main.c.

--- src/main.c
+++ src/main.c
@@ -24,6 +24,13 @@
 #define WHITEBOARD_GLVS { GL_VERTEX_SHADER, "../shaders/whiteboard.glvs" }
 #define WHITEBOARD_GLFS { GL_FRAGMENT_SHADER, "../shaders/whiteboard.glfs" }
 
+#define AIBOARD_W (480)
+#define AIBOARD_H (480)
+#define AIBOARD_X (50)
+#define AIBOARD_Y ((WINDOW_HEIGHT-AIBOARD_H)/2)
+#define AIBOARD_GLVS { GL_VERTEX_SHADER, "../shaders/aiboard.glvs" }
+#define AIBOARD_GLFS { GL_FRAGMENT_SHADER, "../shaders/aiboard.glfs" }
+
 int running;
 
 int main(int argc, char **argv)
@@ -38,14 +45,28 @@ int main(int argc, char **argv)
         identity_matrix(model);
         orthographic_matrix(projection, 0.0, WINDOW_WIDTH, WINDOW_HEIGHT, 0.0, 10.0, -10.0);
 
-        shader_info_t shader_info[3] = {
+        shader_info_t whiteboard_sinfo[3] = {
                 WHITEBOARD_GLVS,
                 WHITEBOARD_GLFS,
                 NO_SHADER
         };
 
-        rect_t rec;
-        init_rect(&rec, WHITEBOARD_X, WHITEBOARD_Y, WHITEBOARD_W, WHITEBOARD_H, shader_info);
+        rect_t whiteboard;
+        init_rect(&whiteboard, WHITEBOARD_X, WHITEBOARD_Y, WHITEBOARD_W, WHITEBOARD_H, whiteboard_sinfo);
+
+        if (had_error_debug()) {
+                fprintf(stderr, "ERROR: %s\n", pop_error_debug());
+                exit(EXIT_FAILURE);
+        }
+
+        shader_info_t aiboard_sinfo[3] = {
+                AIBOARD_GLVS,
+                AIBOARD_GLFS,
+                NO_SHADER
+        };
+
+        rect_t aiboard;
+        init_rect(&aiboard, AIBOARD_X, AIBOARD_Y, AIBOARD_W, AIBOARD_H, aiboard_sinfo);
 
         if (had_error_debug()) {
                 fprintf(stderr, "ERROR: %s\n", pop_error_debug());
@@ -62,11 +83,13 @@ int main(int argc, char **argv)
                         }
                 }
                 clear_buffers_window(GL_COLOR);
-                render_rect(&rec);
+                render_rect(&whiteboard);
+                render_rect(&aiboard);
                 swap_window();
         }
 
-        free_rect(&rec);
+        free_rect(&whiteboard);
+        free_rect(&aiboard);
         flush_debug();
         free_window();
         return 0;
